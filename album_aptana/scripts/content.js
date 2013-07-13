bee3b32debe1919a807e513db81fc586c175a2d7
Macronaut123 <marcos.vinicius.eua@gmail.com>
2013-07-13 12:31:40 -0300

    inserindo album

---
+++ album_aptana/scripts/content.js
@@ -0,0 +1,56 @@
+var musicList = [
+"feathers",
+"forgiven",
+"frontiers",
+"hollywood",
+"hurts",
+"little",
+"lough",
+"oldtown",
+"only",
+"radio",
+"runaway",
+"side",
+"what",
+"young"];
+
+var musicNames = [
+"Toss the Feathers",
+"Forgiven not Forgotten",
+"No Frontiers",
+"Queen of Hollywood",
+"Everybody Hurts",
+"Little Wing",
+"Lough Erin Shore",
+"Old Town",
+"Only When I Sleep",
+"Radio",
+"Runaway",
+"At Your Side",
+"What Can I Do",
+"So Young"];
+
+var albumAmor = [
+'foto0',
+'foto1',
+'foto2',
+'foto3',
+'foto4',
+'foto5',
+'foto6',
+'foto7',
+'foto8',
+'foto9'];
+
+var albumAmor2 = [
+'foto0',
+'foto1',
+'foto2',
+'foto3',
+'foto4',];
+
+var poemList=[
+"",
+"Te amo amor da minha vida, amor da minha vida querida, dentre mil jazidas, foi só você que realmente apareceu para mim - Marcos",
+"Se existe amor no mundo, não sabia o que significava, pelo menos não até te conhecer - Marcos"];
+
---
+++ album_aptana/scripts/core.js
@@ -0,0 +1,66 @@
+
+var currentMusic;
+var currentPhoto = 0;
+
+function initialize(){
+	addSongs();
+	
+	$('.feathers').css('background-color','#A884AA');
+	
+	if(poemList[currentPhoto] != null && poemList[currentPhoto] != ""){$('.photo_poem').html('"'+poemList[currentPhoto]+'"'); $('.photo_poem').css('display','block');} else if(poemList[currentPhoto] == null || poemList[currentPhoto] == ""){$('.photo_poem').html(''); $('.photo_poem').css('display','none');}
+
+	//console.log(photoNames.length);
+	
+	var currentAlbum = albumAmor2;
+	var currentAlbumName = "albumAmor2"
+}
+
+
+function introRemove(){
+	$('.intro_screen').animate({left:'-100%'},5000,removeAct);
+}
+
+function removeAct(){
+	$('.intro_screen').remove();
+}
+
+function addSongs(){
+	var musicLength = musicList.length;
+	
+	for(var i = 0; i < musicLength; i++){
+		$('.core_screen').append($('<p onClick=setupAudio("'+musicList[i]+'") id="music" class='+musicList[i]+'>'+musicNames[i]+'</p>'));
+	}
+}
+
+function coverFadeInOut(){
+	$('.photo_cover').fadeIn(2000, function(){ $('.photo_cover').fadeOut(2000); $('.photo').attr('src','images/slideshow/'+currentAlbumName+'/foto'+currentPhoto+'.png'); })
+	$('.photo_display').html(currentPhoto);
+	if(poemList[currentPhoto] != null && poemList[currentPhoto] != ""){$('.photo_poem').html('"'+poemList[currentPhoto]+'"'); $('.photo_poem').css('display','block');} else if(poemList[currentPhoto] == null || poemList[currentPhoto] == ""){$('.photo_poem').html(''); $('.photo_poem').css('display','none');}
+}
+
+function changePhoto(direction){
+	switch(direction){
+		case "right":
+		if(currentPhoto < (currentAlbum.length-1)){
+			currentPhoto++;
+			console.log(currentPhoto);
+			coverFadeInOut();
+		}
+		break;
+		case "left":
+		if(currentPhoto > 0){
+			currentPhoto--;
+			coverFadeInOut();
+		}
+		break;
+	}
+}
+
+function setupAudio(songName){
+	currentMusic = songName;
+	
+	$('.audio_system').attr('src','sound/'+songName+'.mp3')
+	$('.core_screen p').css('background-color','#C884CC');
+	$('.'+songName+'').css('background-color','#A884AA');
+}
+
